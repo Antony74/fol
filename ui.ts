ac22a915989e4b3c71cdab31d1843e494f10e67d
Antony <akb@akb.me.uk>
2017-10-05 15:43:31 +0100

    Found a correctly set up tsconfig.json file, and got the parser parsing (there's a parse error because it's set up for numerical calculation not ring algebra, but that next phase of development's for another day).

--- ui.ts
+++ ui.ts
@@ -1,17 +1,30 @@
 
-import './node_modules/@types/node/index.d.ts';
+import * as bnf from 'ebnf-parser';
+import {Jison} from 'jison';
 
-const jison = require('./node_modules/jison/lib/jison.js');
+const oReq = new XMLHttpRequest();
+oReq.addEventListener('load', ready);
+oReq.open('GET', 'ring.jison');
+oReq.send();
 
-const arrRingAxioms: string[] = [
-    'a + (b + c) = (a + b) + c',
-    'a + 0 = a',
-    'a + (-a) = 0',
-    'a + b = b + a',
-    'a × (b × c) = (a × b) × c',
-    'a × 1 = a',
-    '1 × a = a',
-    'a × (b + c) = (a × b) + (a × c)',
-    '(b + c) × a = (b × a) + (c × a)'
-];
+function ready() {
+    const grammar = this.responseText;
+    const cfg = bnf.parse(grammar);
+    const parser = Jison.Generator(cfg, {type: 'lalr'}).createParser();
+
+    const arrRingAxioms: string[] = [
+        'a + (b + c) = (a + b) + c',
+        'a + 0 = a',
+        'a + (-a) = 0',
+        'a + b = b + a',
+        'a × (b × c) = (a × b) × c',
+        'a × 1 = a',
+        '1 × a = a',
+        'a × (b + c) = (a × b) + (a × c)',
+        '(b + c) × a = (b × a) + (c × a)'
+    ];
+
+    parser.parse(arrRingAxioms[0]);
+
+}
 
